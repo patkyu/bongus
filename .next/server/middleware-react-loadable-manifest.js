38942f1e8588df51528e5f3086a8b662798a00fb
patkyu <yourstrulypatkyu@gmail.com>
2023-10-21 11:51:39 +0800

    first commit

---
+++ .next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST={"..\\src\\components\\Partials\\DefaultLayout.jsx -> react-facebook-pixel":{"id":"..\\src\\components\\Partials\\DefaultLayout.jsx -> react-facebook-pixel","files":["static/chunks/node_modules_react-facebook-pixel_dist_fb-pixel_js.js"]}}
